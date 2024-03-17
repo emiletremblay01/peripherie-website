8070ecf67c1f764b75abb01e92caa8fb3ce04ccb
Emile Tremblay <emiletremblay01@gmail.com>
2024-03-17 01:00:32 -0400

    changed routing editions

--- app/editions/[index]/page.tsx
+++ app/editions/[index]/page.tsx
@@ -2,7 +2,7 @@
 import editions from "@/data/editions.json";
 import EditionItem from "@/components/EditionItem";
 
-export default function EditionActuelle() {
+export default function Edition() {
   const editionActuelleRaw = editions.find(
     (edition) => edition.isEditionActuelle,
   );
@@ -11,9 +11,9 @@ export default function EditionActuelle() {
   return (
     <section className="flex-1">
       <div className="h-20 w-full sm:hidden"></div>
-      <hr className="mx-4 mb-1  border-black sm:mt-0" />
-      <div className="mb-16 flex flex-1 flex-col gap-5 px-4 ">
-        <h1 className="mb-24  text-3xl font-bold">Édition actuelle</h1>
+      <hr className="mx-4 mb-1 border-black sm:mt-0" />
+      <div className="mb-16 flex flex-1 flex-col gap-5 px-4">
+        <h1 className="mb-24 text-3xl font-bold">Édition actuelle</h1>
         <div className="xl:mx-auto xl:w-full xl:max-w-7xl">
           <EditionItem data={editionActuelleRaw} />
         </div>
